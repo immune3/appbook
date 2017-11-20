03c17ce621e410f5466b6e2b55f778646660874b
youngdo <mapl003@naver.com>
2017-11-20 16:03:27 +0900

    171120

---
+++ app/src/main/java/com/example/dokdofamily01/CustomViewPager.java
@@ -0,0 +1,35 @@
+package com.example.dokdofamily01;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by heronation on 2017-11-16.
+ */
+
+public class CustomViewPager extends ViewPager {
+    private boolean isPageScrollEnabled = true;
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(ev) && this.isPageScrollEnabled;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev) && this.isPageScrollEnabled;
+    }
+    public void setPageScrollEnabled(boolean isPageScrollEnabled) {
+        this.isPageScrollEnabled = isPageScrollEnabled;
+    }
+}
